07005b125310b7b64a724c3b75ea161150426c0d
Himanshu Koshti <Himfeedback74@gmail.com>
2024-03-18 23:06:04 +0530

    Logback setup

---
+++ src/test/java/com/SpringLog/SpringLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
